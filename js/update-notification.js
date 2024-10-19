320c96ecc7986b0d91c6e15369ca009ddd6a8e71
Cedric Rohm <rohm.cedric@gmail.com>
2024-10-19 21:24:07 +0200

    saving currently used used version when clicking the message dismiss button as lastAckedVersion loading and comparing lastAckedVersion with currently used version and only showing the notification if lastAckedVersion is empty (never used) or it does not match the currently used version

--- js/update-notification.js
+++ js/update-notification.js
@@ -1,16 +1,26 @@
-function createAlert(text) {
+function createAlert(text, version) {
     const divContainer = document.createElement("div");
     divContainer.role = "alert"
     divContainer.classList = "alert systemMessage alert-success alert-dismissible fade show"
 
     const span = document.createElement("span");
-    span.classList = "fonticon-check-circle alert-icon";
+    span.classList = "fonticon-beta-test alert-icon";
 
     const button = document.createElement("button");
     button.type = "button";
     button.setAttribute("data-bs-dismiss", "alert");
     button.ariaLabel = "Close";
     button.classList = "btn-close";
+
+    button.addEventListener("click", function () {
+        browser.storage.local.set({ lastAckedVersion: version })
+            .then(() => {
+                console.log('Currently used version saved: ' + version);
+            })
+            .catch((error) => {
+                console.error('Error saving data:', error);
+            });
+    });
 
     const divContent = document.createElement("div");
     divContent.classList = "alert-content";
@@ -19,16 +29,21 @@ function createAlert(text) {
     h4.classList = "alert-heading";
     h4.textContent = text;
 
-    divContent.append(h4);
+    const a = document.createElement("a");
+    a.href = "https://nudelforce.notion.site/Changelog-119f020626c2802883eededd33cc5fab";
+    a.target = "_blank";
+    a.textContent = "Check out the Changelog";
+
+    divContent.append(h4, a);
 
     divContainer.append(span, button, divContent);
 
     return divContainer;
 }
 
-function makeAnnouncement(text) {
+function makeAnnouncement(text, version) {
     const alertContainer = document.getElementById("AlertContainer");
-    alertContainer.append(createAlert(text));
+    alertContainer.append(createAlert(text, version));
 }
 
 (async function main() {
@@ -37,5 +52,17 @@ function makeAnnouncement(text) {
     const manifestData = browser.runtime.getManifest();
     const version = manifestData.version;
 
-    makeAnnouncement("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.");
+    browser.storage.local.get(['lastAckedVersion'])
+        .then((result) => {
+            const lastAckedVersion = result.lastAckedVersion;
+            console.log('Last acknowledged version: ', lastAckedVersion);
+            console.log('Current version: ', version);
+            if(!lastAckedVersion || lastAckedVersion != version) {
+                makeAnnouncement(`Cardmarket Assistant, new Version: ${version}`, version);
+            }
+        })
+        .catch((error) => {
+            console.error('Error retrieving data:', error);
+        });
+
 })();
